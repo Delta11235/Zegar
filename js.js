b9a0882f06d0eb778ac0f901b1483517d62651d0
Delta11235 <mateusz._01@wp.pl>
2023-03-09 00:44:15 +0100

    asdzx

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
 function Kolor() {
     document.getElementById("kwadrat").style.backgroundColor = "red";
 }
+function Kolor2() {
+    document.getElementById("kwadrat").style.backgroundColor = "white";
+}
