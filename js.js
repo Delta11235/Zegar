0943013fa611fed3258517bf7928b9457fd4d69d
Delta11235 <mateusz._01@wp.pl>
2023-03-09 18:10:00 +0100

    zegar dziala

--- js.js
+++ js.js
@@ -1,6 +1,36 @@
-function Kolor() {
-    document.getElementById("kwadrat").style.backgroundColor = "red";
+function Czas() {
+    const today = new Date();
+    let godz = today.getHours();
+    let min = today.getMinutes();
+    let sek = today.getSeconds();
+
+    document.getElementById("godz").innerHTML = godz;
+
+    if(sek < 10){
+        document.getElementById("sek").innerHTML = ("0"+sek);
+    }
+    else{
+    document.getElementById("sek").innerHTML = sek;
+    }
+    if(min < 10){
+        document.getElementById("min").innerHTML = ("0"+min);
+    }
+    else{
+    document.getElementById("min").innerHTML = min;
+    }
+    if(godz < 10){
+        document.getElementById("godz").innerHTML = ("0"+godz);
+    }
+    else{
+    document.getElementById("godz").innerHTML = godz;
+    }
+    
 }
-function Kolor2() {
-    document.getElementById("kwadrat").style.backgroundColor = "white";
+
+function F5() {
+    setTimeout(() => {
+        Czas();
+        F5();
+    }, 1000);
 }
+
